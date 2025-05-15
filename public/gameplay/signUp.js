428cafc91004e71312c071794e88897642fb53ac
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-05-15 11:06:34 -0500

    Added profile pic changer, saves the variable but doesn't pass it to the gameplay or lobbies

--- public/gameplay/signUp.js
+++ public/gameplay/signUp.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+function listen(event, element, callback) {
+  return element.addEventListener(event, callback);
+}
+
 // Get the DOM element for the 'select-name' button
 document.querySelector('.select-name').addEventListener('click', () => {
   const nickname = document.getElementById('playerName').value.trim();
@@ -14,4 +22,31 @@ document.querySelector('.select-name').addEventListener('click', () => {
 
   // Redirect to game page
   window.location.href = "./index.html";
+});
+
+let profilePic = "";
+let imageIndex = 0;
+const avatar = select(".avatar");
+const rightArrow = select(".right-arrow");
+const leftArrow = select(".left-arrow");
+
+const imageFiles = ["../img/ded.png", "../img/devil.png", "../img/happy.png", "../img/star.png", "../img/think.png", "../img/wink.png", "../img/worry.png"];
+avatar.src = imageFiles[imageIndex];
+listen("click", rightArrow,() => {
+  imageIndex++;
+  console.log("imageIndex");
+  if (imageIndex === 6) {
+    imageIndex = 0;
+  }
+  profilePic = imageFiles[imageIndex];
+  avatar.src = profilePic;
+});
+
+listen("click", leftArrow,() => {
+  imageIndex--;
+  if (imageIndex === 0) {
+    imageIndex = 6;
+  }
+  profilePic = imageFiles[imageIndex];
+  avatar.src = profilePic;
 });
